d4ff5d96f8d69a7c34f2c1e60f1e292bb0379697
Jakub <j.jajkowicz@softteco.com>
2025-09-16 10:57:59 +0200

    bugfix

--- frontend/src/lib/medplum.ts
+++ frontend/src/lib/medplum.ts
@@ -1,7 +1,5 @@
 import { MedplumClient } from "@medplum/core";
 
 export const medplum = new MedplumClient({
-    clientId: process.env.NEXT_PUBLIC_MEDPLUM_CLIENT_ID,
-    clientSecret: process.env.NEXT_PUBLIC_MEDPLUM_CLIENT_SECRET,
     baseUrl: process.env.NEXT_PUBLIC_MEDPLUM_BASE_URL,
 });
